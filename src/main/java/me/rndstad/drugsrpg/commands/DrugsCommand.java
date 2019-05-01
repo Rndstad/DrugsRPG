ba4d853aa5bd47372a7f850955ff58726297575d
Rndstad <rndstad007@gmail.com>
2019-05-01 23:41:49 +0200

    Extra command /drugs give <player> <drug> v3.3

--- src/main/java/me/rndstad/drugsrpg/commands/DrugsCommand.java
+++ src/main/java/me/rndstad/drugsrpg/commands/DrugsCommand.java
@@ -5,6 +5,7 @@ import me.rndstad.drugsrpg.builder.menus.BuilderInventory;
 import me.rndstad.drugsrpg.common.tools.ChatUtils;
 import me.rndstad.drugsrpg.consume.Drug;
 import me.rndstad.drugsrpg.consume.menus.DrugInventory;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -22,7 +23,7 @@ public class DrugsCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            if (args.length > 0) {
+            if (args.length == 1) {
                 if (args[0].equalsIgnoreCase("menu")) {
                     if (player.hasPermission("drugsrpg." + args[0]) || player.hasPermission("drugsrpg.*")) {
                         DrugInventory.INVENTORY.open(player);
@@ -31,6 +32,23 @@ public class DrugsCommand implements CommandExecutor {
                 if (args[0].equalsIgnoreCase("create")) {
                     if (player.hasPermission("drugsrpg." + args[0]) || player.hasPermission("drugsrpg.*")) {
                         BuilderInventory.INVENTORY.open(player);
+                    }
+                }
+            } else if (args.length == 3) {
+                if (args[0].equalsIgnoreCase("give")) {
+                    String targetName = args[1];
+                    Player target = Bukkit.getPlayer(targetName);
+                    if (target != null) {
+                        String drugName = args[2];
+                        Drug drug = drugsrpg.getDrugsManager().getDrug(drugName);
+                        if (drug != null) {
+                            target.getInventory().addItem(drug.getItemStack());
+                            player.sendMessage(ChatUtils.format("&aYou gave " + drug.getName() + " to " + target.getName() + "."));
+                        } else {
+                            player.sendMessage(ChatUtils.format("&aThis drug isn't found in the database."));
+                        }
+                    } else {
+                        player.sendMessage(ChatUtils.format("&aThis player isn't found in the server."));
                     }
                 }
             }
