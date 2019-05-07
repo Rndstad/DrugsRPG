c5c2afcbc8d05e40f03622c51974b1696377d7d8
Rndstad <rndstad007@gmail.com>
2019-05-07 14:41:49 +0200

    Notifier for updates!

--- src/main/java/me/rndstad/drugsrpg/DrugsPlugin.java
+++ src/main/java/me/rndstad/drugsrpg/DrugsPlugin.java
@@ -4,14 +4,18 @@ import fr.minuskube.inv.InventoryManager;
 import me.rndstad.drugsrpg.builder.BuilderManager;
 import me.rndstad.drugsrpg.commands.DrugsCommand;
 import me.rndstad.drugsrpg.common.tools.Metrics;
+import me.rndstad.drugsrpg.common.tools.VersionUtils;
 import me.rndstad.drugsrpg.common.tools.YamlConfig;
 import me.rndstad.drugsrpg.consume.DrugsManager;
 import me.rndstad.drugsrpg.database.DatabaseManager;
 import me.rndstad.drugsrpg.glow.GlowManager;
 import me.rndstad.drugsrpg.placeholder.PlaceholderManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class DrugsPlugin extends JavaPlugin {
+
+    private String currentVersion;
 
     private YamlConfig config;
     private YamlConfig drugs;
@@ -28,6 +32,9 @@ public class DrugsPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         super.onEnable();
+
+        currentVersion = getDescription().getVersion();
+        checkUpdates();
 
         config = new YamlConfig(this, "config.yml", true);
         if (!config.getConfig().getBoolean("preferences.use-mysql")) drugs = new YamlConfig(this, "drugs.yml", true);
@@ -57,6 +64,27 @@ public class DrugsPlugin extends JavaPlugin {
                 databaseManager.getPool().close();
             }
         }
+    }
+
+    private void checkUpdates() {
+        if (!getConfigFile().getConfig().getBoolean("check-for-updates", true)) {
+            return;
+        }
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                String latestVersion = VersionUtils.getLatestSpigotVersion("39887");
+                if (latestVersion == "error") {
+                    getLogger().info("[DrugsRPG] Error attempting to check for new version. Make sure you report this problem here: https://www.spigotmc.org/threads/drugsrpg-flatfile-mysql-support-added-1-8-1-12-2.235099/");
+                } else {
+                    if (!currentVersion.equals(latestVersion)) {
+                        getLogger().info("[DrugsRPG] Howdy! There is version " + latestVersion + " available on Spigot: https://www.spigotmc.org/resources/drugsrpg-flatfile-mysql-support-added-1-8-1-12-2.39887/");
+                        getLogger().info("[DrugsRPG] Make sure you download this version for the best experience!");
+                    }
+                }
+            }
+        }.runTaskLaterAsynchronously(this, 40L);
     }
 
     public DrugsManager getDrugsManager() {
